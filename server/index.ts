a5e75ba1ee3e84fbbb190fce5de5c4dab23c4c58
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-04-06 08:12:50 +0900

    server: first commit

---
+++ server/index.ts
@@ -0,0 +1,28 @@
+import * as trpcExpress from "@trpc/server/adapters/express";
+import express from "express";
+import { initTRPC } from "@trpc/server";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const t = initTRPC.create({});
+
+const appRouter = t.router({
+  greetings: t.procedure.query(() => {
+    return "hello world :^)";
+  }),
+});
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+  })
+);
+
+app.listen(PORT, () => console.log(`🌞🌞🌞 Listening on ${PORT} 🌞🌞🌞`));
+
+export type AppRouter = typeof appRouter;
