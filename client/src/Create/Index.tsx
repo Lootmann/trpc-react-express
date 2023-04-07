ba4a82483a5d662e80af61951bc725db6baa77a0
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-04-07 13:28:32 +0900

    client: create user form using mutation

--- client/src/Create/Index.tsx
+++ client/src/Create/Index.tsx
@@ -1,9 +1,59 @@
 import { trpc } from "../trpc";
+import { useState } from "react";
 
 export function Index() {
-  const msg = trpc.create.useQuery({ name: "hoge" });
-  console.log(msg);
-  console.log(msg.data);
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
 
-  return <div>{msg.data}</div>;
+  const createMutation = trpc.createUser.useMutation();
+
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    const { name, value } = e.target;
+    if (name == "name") {
+      setName(value);
+    } else if (name == "email") {
+      setEmail(value);
+    }
+  }
+
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    createMutation.mutate({ name: name, email: email });
+  }
+
+  return (
+    <div className="w-1/2">
+      <form
+        method="post"
+        onSubmit={(e) => onSubmit(e)}
+        className="flex flex-col gap-5"
+      >
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={(e) => onChange(e)}
+          className="bg-zinc-700 text-zinc-200 px-4 rounded-md"
+          placeholder="Your Name"
+        />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => onChange(e)}
+          className="bg-zinc-700 text-zinc-200 px-4 rounded-md"
+          placeholder="Your Email"
+        />
+
+        <input
+          type="submit"
+          value="+ Create"
+          className="bg-zinc-700 hover:bg-green-800 transition-all duration-200"
+        />
+      </form>
+    </div>
+  );
 }
--- client/src/ReactQuery/Index.tsx
+++ client/src/ReactQuery/Index.tsx
@@ -2,13 +2,25 @@ import { trpc } from "../trpc";
 
 export function Index() {
   const greetings = trpc.greetings.useQuery();
+  const users = trpc.users.useQuery();
 
   return (
-    <div className="flex">
-      tRPC with ReactQuery:
-      <span className="bg-zinc-200 text-zinc-900 border px-2 ml-5 rounded-md">
-        {greetings.data}
-      </span>
+    <div className="flex flex-col gap-10">
+      <div className="flex border p-2 rounded-md">
+        <h1>tRPC with ReactQuery:</h1>
+
+        <span className="bg-zinc-200 text-zinc-900 border px-2 ml-5 rounded-md">
+          {greetings.data}
+        </span>
+      </div>
+
+      <div className="border p-2 rounded-md">
+        {users.data?.map((user) => (
+          <p key={user.id}>
+            {user.id}, {user.name}, {user.email}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
